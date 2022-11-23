6b285c4c31b85786f7bf362ac29d600950ceb60e
Praveen Kumar <praveen170596@gmail.com>
2022-11-23 07:51:01 +0530

    created Music class

---
+++ src/main/java/com/niit/jdp/BEJ_C2_S1_REST_MySQL_MC_1/domain/Music.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name:Praveen Kumar
+ *Date: 23-Nov-22
+ *Created With IntelliJ Idea Community Edition
+ */
+
+
+package com.niit.jdp.BEJ_C2_S1_REST_MySQL_MC_1.domain;
+
+public class Music {
+}
